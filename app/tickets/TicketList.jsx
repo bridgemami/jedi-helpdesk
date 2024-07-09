b78d95976cce93a3441e59f9b1617fbb6d8ad7be
Michael <80859948+bridgemami@users.noreply.github.com>
2024-07-09 11:17:57 -0700

    added dynamic routing for each ticket

--- app/tickets/TicketList.jsx
+++ app/tickets/TicketList.jsx
@@ -1,3 +1,4 @@
+import Link from "next/link"
 async function getTickets() {
     const res= await fetch('http://localhost:4000/tickets', {
         next: {
@@ -14,9 +15,11 @@ const tickets= await getTickets()
    <>
    {tickets.map(ticket => (
     <section key = {ticket.id} className="card my-5">
+        <Link href={`tickets/${ticket.id}`}>
         <h3>{ticket.title}</h3>
         <p>{ticket.body.slice(0,200)}...</p>
         <div className={`pill ${ticket.priority}`}>priority</div>
+        </Link>
     </section>
    ))}
    </>
