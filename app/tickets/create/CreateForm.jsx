01525b234d97b22363915f021dc5ce98a5d511c2
Michael <80859948+bridgemami@users.noreply.github.com>
2024-07-11 17:05:32 -0700

    create a new ticket component

---
+++ app/tickets/create/CreateForm.jsx
@@ -0,0 +1,68 @@
+"use client";
+//when do not want client side rendering
+//use it very min
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+export default function CreateForm() {
+    const router = useRouter()
+
+  const [title,setTitlle] =useState('')
+  const [body, setBody] = useState('')
+  const [priority, setPriority] = useState('low')
+  const [isLoading, setIsLoading] = useState(false)
+  //async b/c it is interacting wih data
+  const handleSubmit = async (e) =>{
+        e.preventDefault()
+        setIsLoading(true)
+        const ticket = {
+            title,body, priority, user_email: 'yoda@jedi.com',
+        }
+        const res = await fetch("http://localhost:4000/tickets",{ 
+            method: "POST",
+            header: {"Content-Type": "application/json"},
+            body: JSON.stringify(ticket)        
+        }
+        );
+        if(res.status === 201) {
+            router.refresh() // to refresh data
+            router.push('/tickets')
+        }
+  }
+  
+    return (
+      <form onSubmit={handleSubmit} className="w-1/2">
+        <label>
+          <span>Title:</span>
+          <input
+            required
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+          />
+        </label>
+        <label>
+          <span>Title:</span>
+          <textarea
+            required
+            onChange={(e) => setBody(e.target.value)}
+            value={body}
+          />
+        </label>
+        <label>
+          <span>Priority:</span>
+          <select
+            onChange={(e) => setPriority(e.target.value)}
+            value={priority}
+          >
+            <option value="low">Low Priority</option>
+            <option value="medium">Medium Priority</option>
+            <option value="high">High Priority</option>
+          </select>
+        </label>
+        <button className="btn-primary" disabled={isLoading}>
+          {isLoading && <span>Adding...</span>}
+          {!isLoading && <span>Add Ticket</span>}
+        </button>
+      </form>
+    );
+}
